824d34fb1f97daad371f7d8380281a41e1c0badb
nafri-irfan96 <nafri.irfan96@gmaail.com>
2015-12-01 15:09:03 +0530

    Submit Commit

--- src/NumbertoStr.cpp
+++ src/NumbertoStr.cpp
@@ -18,8 +18,86 @@ NOTES: Don't create new string.
 */
 
 #include <stdio.h>
+#include <string.h>
+#include <malloc.h>
 
 
 void number_to_str(float number, char *str,int afterdecimal){
-	
+	int temp, i = 0;
+
+	*str = NULL;
+	if (afterdecimal == 0)
+	{
+		temp = (int)number;
+		temp = (temp < 0) ? -temp : temp;
+
+		while (temp != 0)
+		{
+			str[i] = temp % 10 + '0';
+			temp /= 10;
+			i++;
+		}
+		str[i] = '\0';
+		strrev(str);
+		if (number < 0)
+		{
+			for (i = strlen(str); i >= 1; i--)
+			{
+				str[i] = str[i - 1];
+			}
+			str[strlen(str) + 1] = '\0';
+			str[0] = '-';
+		}
+	}
+	if (afterdecimal != 0)
+	{
+		temp = (int)number;
+		temp = (temp < 0) ? -temp : temp;
+		int mul = 1;
+		i = 0;
+		while (temp != 0)
+		{
+			str[i] = temp % 10 + '0';
+			temp /= 10;
+			i++;
+		}
+		str[i] = '\0';
+		strrev(str);
+		str[i] = '.';
+		str[i + 1] = '\0';
+
+
+		char *t;
+		t = (char*)malloc((afterdecimal)*sizeof(char));
+		float temp2;
+		temp2 = number - (int)number;
+		while (afterdecimal != 0)
+		{
+			mul *= 10;
+			afterdecimal--;
+		}
+		temp2 *= mul;
+		int stringadder = (int)temp2;
+		stringadder = stringadder < 0 ? -stringadder : stringadder;
+		i = 0;
+		while (stringadder != 0)
+		{
+			t[i] = stringadder % 10 + '0';
+			stringadder /= 10;
+			i++;
+		}
+		t[i] = '\0';
+		strrev(t);
+		strcat(str, t);
+
+		if (number < 0)
+		{
+			for (i = strlen(str); i >= 1; i--)
+			{
+				str[i] = str[i - 1];
+			}
+			str[strlen(str)] = '\0';
+			str[0] = '-';
+		}
+	}
 }
