a4ce12dcb8df64192469b025f7972d15bdccd867
nafri-irfan96 <nafri.irfan96@gmaail.com>
2015-12-02 14:38:57 +0530

    Submit Commit

--- src/removeSpaces.cpp
+++ src/removeSpaces.cpp
@@ -10,7 +10,31 @@ ERROR CASES: Return '\0' for invalid inputs.
 
 NOTES: Don't create new string.
 */
-
+#include<string.h>
 char removeSpaces(char *str) {
-	return '\0';
+	if (str == NULL || str == "")
+		return '\0';
+	int l = strlen(str);
+	int i = 0, j = 0, k;
+	j = i;
+	while (str[i] != '\0')
+	{
+		
+		if (str[i] != ' ')
+			i++;
+		else
+		{
+			j = i;
+			k = i;
+			while (str[j] == ' ')
+				j++;
+			for (; j <= strlen(str); j++)
+			{
+				str[k] = str[j];
+				k++;
+			}
+		}
+
+	}
+
 }
